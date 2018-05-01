7eda0dafe19aa9168083e76970646cb064fd2116
Eric Audet <eric@savi.ca>
2018-05-01 15:33:40 +0000

    arango

---
+++ dd-elas.sh
@@ -0,0 +1,3 @@
+dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el1
+dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el2 skip=76800
+dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el3 skip=153600
---
+++ dd-kibana.sh
@@ -0,0 +1,7 @@
+dd if=../kibana.tar.gz bs=1024 count=76800 of=ki1
+dd if=../kibana.tar.gz bs=1024 count=76800 of=ki2 skip=76800
+dd if=../kibana.tar.gz bs=1024 count=76800 of=ki3 skip=153600
+dd if=../kibana.tar.gz bs=1024 count=76800 of=ki4 skip=230400
+dd if=../kibana.tar.gz bs=1024 count=76800 of=ki5 skip=307200
+dd if=../kibana.tar.gz bs=1024 count=76800 of=ki6 skip=384000
+dd if=../kibana.tar.gz bs=1024 count=76800 of=ki7 skip=460800

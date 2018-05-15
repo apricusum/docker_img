e43a8c87b1d088f33ace6fdf453678e830fc52ae
Eric Audet <eric@savi.ca>
2018-05-15 18:30:31 +0000

    kafka cluster

---
+++ dd-kafka.sh
@@ -0,0 +1,10 @@
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka1
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka2 skip=76800
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka3 skip=153600
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka4 skip=230400
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka5 skip=307200
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka6 skip=384000
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka8 skip=460800
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka9 skip=537600
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka10 skip=614400
+dd if=../kafka.tar.gz bs=1024 count=76800 of=ka11 skip=691200
---
+++ dd-kafka_cluster.sh
@@ -0,0 +1,2 @@
+dd if=../kafka_cluster.tar.gz bs=1024 count=76800 of=kc1
+dd if=../kafka_cluster.tar.gz bs=1024 count=76800 of=kc2 skip=76800

031472475c5ef4cd28afe539df4f8a080c61b362
Éric Audet <eric.audet@intact.net>
2018-05-16 14:56:48 -0400

    sanitized

--- dd-glue-zookeeper.sh
+++ dd-glue-zookeeper.sh
@@ -1,3 +1,2 @@
 dd if=zo1 of=../zookeeper.tar.gz bs=1024 count=76800
 dd if=zo2 of=../zookeeper.tar.gz bs=1024 count=76800 seek=76800
-dd if=zo3 of=../zookeeper.tar.gz bs=1024 count=76800 seek=153600

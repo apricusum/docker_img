dcc4db5b7ceb0f1f460b185013c45e6a715c0cf5
Eric Audet <eric@savi.ca>
2018-05-16 18:54:43 +0000

    zookeeper

--- dd-zookeeper.sh
+++ dd-zookeeper.sh
@@ -1,4 +1,2 @@
 dd if=../zookeeper.tar.gz bs=1024 count=76800 of=zo1
 dd if=../zookeeper.tar.gz bs=1024 count=76800 of=zo2 skip=76800
-dd if=../zookeeper.tar.gz bs=1024 count=76800 of=zo3 skip=153600
-dd if=../zookeeper.tar.gz bs=1024 count=76800 of=zo4 skip=230400

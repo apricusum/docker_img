f8ec913ef81e36d8f9491e591a20f34dd7830838
Eric Audet <eric@savi.ca>
2018-05-22 18:44:24 +0000

    kafkamanager

---
+++ dd-kafkamanager.sh
@@ -0,0 +1,3 @@
+dd if=../kafkamanager.tar.gz bs=1024 count=76800 of=km1
+dd if=../kafkamanager.tar.gz bs=1024 count=76800 of=km2 skip=76800
+dd if=../kafkamanager.tar.gz bs=1024 count=76800 of=km3 skip=153600

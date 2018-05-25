6befe2406dbe4d775fbf003e9d7811769d48580a
Eric Audet <eric@savi.ca>
2018-05-25 18:07:32 +0000

    zoonapi

---
+++ dd-zooapi.sh
@@ -0,0 +1,5 @@
+dd if=../zooapi.tar.gz bs=1024 count=76800 of=za1
+dd if=../zooapi.tar.gz bs=1024 count=76800 of=za2 skip=76800
+dd if=../zooapi.tar.gz bs=1024 count=76800 of=za3 skip=153600
+dd if=../zooapi.tar.gz bs=1024 count=76800 of=za4 skip=230400
+dd if=../zooapi.tar.gz bs=1024 count=76800 of=za5 skip=307200

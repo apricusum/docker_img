0f8e9f4fd20cb586c4d3727ca3b0d8a53ea138f8
Éric Audet <eric.audet@intact.net>
2018-05-25 14:15:31 -0400

    sanitized

---
+++ dd-glue-kafkamanager.sh
@@ -0,0 +1,3 @@
+dd if=km1 of=../kafkamanager.tar.gz bs=1024 count=76800
+dd if=km2 of=../kafkamanager.tar.gz bs=1024 count=76800 seek=76800
+dd if=km3 of=../kafkamanager.tar.gz bs=1024 count=76800 seek=153600
---
+++ dd-glue-zooapi.sh
@@ -0,0 +1,5 @@
+dd if=za1 of=zooapi.tar.gz bs=1024 count=76800
+dd if=za2 of=zooapi.tar.gz bs=1024 count=76800 seek=76800
+dd if=za3 of=zooapi.tar.gz bs=1024 count=76800 seek=153600
+dd if=za4 of=zooapi.tar.gz bs=1024 count=76800 seek=230400
+dd if=za5 of=zooapi.tar.gz bs=1024 count=76800 seek=307200

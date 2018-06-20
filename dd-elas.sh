c481308763f75a55f64edc77af3f3b0b386c7c4b
Eric Audet <eric@savi.ca>
2018-06-20 14:05:55 +0000

    elastic

--- dd-elas.sh
+++ dd-elas.sh
@@ -1,3 +1,5 @@
 dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el1
 dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el2 skip=76800
 dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el3 skip=153600
+dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el4 skip=230400
+dd if=../elasticsearch.tar.gz bs=1024 count=76800 of=el5 skip=307200

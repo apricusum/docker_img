519fd5b201cee5cebb2df295cef3f9bde7d08bab
Éric Audet <eric.audet@intact.net>
2018-06-20 10:21:59 -0400

    sanitazed

--- dd-glue-el.sh
+++ dd-glue-el.sh
@@ -1,3 +1,5 @@
 dd if=el1 of=elasticsearch.tar.gz bs=1024 count=76800
 dd if=el2 of=elasticsearch.tar.gz bs=1024 count=76800 seek=76800
 dd if=el3 of=elasticsearch.tar.gz bs=1024 count=76800 seek=153600
+dd if=el4 of=elasticsearch.tar.gz bs=1024 count=76800 seek=230400
+dd if=el5 of=elasticsearch.tar.gz bs=1024 count=76800 seek=307200

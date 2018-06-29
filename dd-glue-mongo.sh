b01ef2f772822a46ada25d637f5c9366ff106e95
Eric Audet <eric@savi.ca>
2018-06-29 15:35:06 +0000

    mongo

---
+++ dd-glue-mongo.sh
@@ -0,0 +1,2 @@
+dd if=mo1 of=mongo.tar.gz bs=1024 count=76800
+dd if=mo2 of=mongo.tar.gz bs=1024 count=76800 seek=76800
---
+++ dd-mongo.sh
@@ -0,0 +1,2 @@
+dd if=../mongo.tar.gz bs=1024 count=76800 of=mo1
+dd if=../mongo.tar.gz bs=1024 count=76800 of=mo2 skip=76800
